b3e313bafb2f491110d4d520eb8302e2778e031b
Tarlan <babalgoogool@gmail.com>
2019-04-22 10:16:56 +0430

    Heroes list finished, Cell class created

--- src/Model/Hero.java
+++ src/Model/Hero.java
@@ -1,6 +1,7 @@
 package Model;
 
 public class Hero extends Card {
+    private int cooldown ;
     public Hero(int id, String[] info, Buff... buffs) {
         super(id, info, buffs);
     }
--- src/Model/Minion.java
+++ src/Model/Minion.java
@@ -1,6 +1,7 @@
 package Model;
 
 public class Minion extends Card {
+
     public Minion(int id, String[] info, Buff... buffs) {
         super(id, info, buffs);
     }
