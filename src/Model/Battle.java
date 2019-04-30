3e233784fc282b5114cefe4c7114bc9f8ef4f12d
neginjafariii <neginjfi79@gmail.com>
2019-04-30 18:27:46 +0430

    Story sho daram ok mikonm

--- src/Model/Battle.java
+++ src/Model/Battle.java
@@ -1029,5 +1029,14 @@ public class Battle {
         }
     }
 
+    public Coordinate setCardCoordinates(Card card) {
+        if (getFieldCards().length == 0) {
+            return new Coordinate(6, 2);
+        } else {
+            return new Coordinate(getFieldCards()[1][getFieldCards().length - 1].getCoordinate().getX(), (getField()[1][getFieldCards().length - 1].getCoordinate().getY() + 1) % 5);
+        }
+    }
+
+   
 
 }
