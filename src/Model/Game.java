90b128f239eacde8141efd9b3d7908453a55b0e2
yasharzb <yasharzb@ce.sharif.edu>
2019-05-06 16:37:50 +0430

    Account objects refactoring reviewed.

--- src/Model/Game.java
+++ src/Model/Game.java
@@ -158,53 +158,28 @@ public class Game {
     }
 
     private void accountObjectInitializer(Account account) {
-        Collection temp = new Collection();
-        for (Card card : account.getCollection().getCards()) {
-            for (Deck deck : account.getCollection().getDecks()) {
-                if (Card.getCardByID(card.getId(), deck.getCards().toArray(new Card[deck.getCards().size()])) != null)
-                    continue;
-                temp.getCards().add(card);
-            }
-        }
-        for (Item item : account.getCollection().getItems()) {
-            for (Deck deck : account.getCollection().getDecks()) {
-                if (Item.getItemByID(item.getId(), deck.getItem()) != null)
-                    continue;
-                temp.getItems().add(item);
-            }
-        }
-        account.getCollection().getCards().clear();
-        account.getCollection().getItems().clear();
-        accountCardRefactor(account.getCollection(), temp.getCards().toArray(new Card[temp.getCards().size()]));
-        accountItemRefactor(account.getCollection(), temp.getItems().toArray(new Item[temp.getItems().size()]));
-        for (Deck deck : account.getCollection().getDecks()) {
-            accountCardRefactor(account.getCollection(), deck.getCards().toArray(new Card[deck.getCards().size()]));
-            accountItemRefactor(account.getCollection(), deck.getItem());
-        }
-    }
-
-    private void accountCardRefactor(Collection collection, Card... cards) {
-        for (int i = 0; i < cards.length; i++) {
-            switch (cards[i].getClass().getName()) {
+        sortCards(account.getCollection().getCards());
+        sortItems(account.getCollection().getItems());
+        Collection collection = account.getCollection();
+        for (int i = 0; i < collection.getCards().size(); i++) {
+            switch (collection.getCards().get(i).getClass().getName()) {
                 case "Hero":
-                    cards[i].setId(++lastHeroId);
+                    if (i + 1 == collection.getCards().size())
+                        lastHeroId = collection.getCards().get(i).getId();
                     break;
                 case "Minion":
-                    cards[i].setId(++lastMinionId);
+                    if (i + 1 == collection.getCards().size()
+                            || !collection.getCards().get(i + 1).getClass().getName().equals("Minions"))
+                        lastMinionId = collection.getCards().get(i).getId();
                     break;
                 case "Spell":
-                    cards[i].setId(++lastSpellId);
+                    if (i + 1 == collection.getCards().size()
+                            || !collection.getCards().get(i + 1).getClass().getName().equals("Spell"))
+                        lastSpellId = collection.getCards().get(i).getId();
                     break;
             }
-            collection.getCards().add(cards[i]);
         }
-    }
-
-    private void accountItemRefactor(Collection collection, Item... items) {
-        for (int i = 0; i < items.length; i++) {
-            items[i].setId(++lastItemId);
-            collection.getItems().add(items[i]);
-        }
+        lastItemId = account.getCollection().getItems().get(account.getCollection().getItems().size() - 1).getId();
     }
 
     private void sortCards(ArrayList<Card> cards) {
