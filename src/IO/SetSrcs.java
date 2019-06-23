7e7826c74a883844e78faba66b90ba77df061879
yasharzb <yasharzb@ce.sharif.edu>
2019-06-23 23:46:26 +0430

    Setting item sources is added.

--- src/IO/SetSrcs.java
+++ src/IO/SetSrcs.java
@@ -13,16 +13,23 @@ public class SetSrcs {
         setSrc("Cards/Heroes");
         setSrc("Cards/Spells");
         setSrc("Cards/Minions");
+        setSrc("Items");
     }
 
     private static void setSrc(String type) throws Exception {
         File dir = new File("./src/Objects/" + type);
-        File gifDir = new File("gifs/" + type.split("/")[1]);
+        File gifDir = null;
+        try {
+            gifDir = new File("gifs/" + type.split("/")[1]);
+        } catch (Exception e) {
+            gifDir = new File("gifs/" + type);
+        }
         BufferedReader list = new BufferedReader(new FileReader("./resources/" + gifDir + "/list.txt"));
         if (dir.exists()) {
             if (dir.isDirectory()) {
                 for (File file : dir.listFiles()) {
                     if (file.isFile()) {
+                        System.out.println(file.getName());
                         BufferedReader reader = new BufferedReader(new FileReader(file));
                         String json = null;
                         String name = null;
@@ -58,6 +65,11 @@ public class SetSrcs {
                                 json = new Gson().toJson(spell);
                                 break;
                             case "Items":
+                                Item item = new Gson().fromJson(reader, Item.class);
+                                item.setIdleSrc(gifDir + "/" + list.readLine());
+                                item.setAttackSrc(gifDir + "/" + list.readLine());
+                                name = item.getName();
+                                json = new Gson().toJson(item);
                                 break;
                         }
                         try {
