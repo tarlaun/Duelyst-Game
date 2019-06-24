5a0250005ddb4d636f99111b791c16786f75c602
yasharzb <yasharzb@ce.sharif.edu>
2019-06-24 21:14:43 +0430

    Card names added.

--- src/View/CardView.java
+++ src/View/CardView.java
@@ -17,6 +17,7 @@ public class CardView {
     private Label power;
     private Label health;
     private Label type;
+    private Label name;
     private AnchorPane pane = new AnchorPane();
 
     public CardView(Card card) {
@@ -27,12 +28,16 @@ public class CardView {
                     template = new ImageView(new Image("card_backgrounds/craftable_unit@2x.png"));
                     power = new Label(Integer.toString(card.getAssaultPower()));
                     health = new Label(Integer.toString(card.getHealthPoint()));
+                    power.translateXProperty().bind(power.widthProperty().divide(2).negate());
+                    health.translateXProperty().bind(health.widthProperty().divide(2).negate());
                     break;
                 case "Minion":
                     character = new ImageView(new Image(card.getIdleSrc()));
                     template = new ImageView(new Image("card_backgrounds/craftable_unit@2x.png"));
                     power = new Label(Integer.toString(card.getAssaultPower()));
                     health = new Label(Integer.toString(card.getHealthPoint()));
+                    power.translateXProperty().bind(power.widthProperty().divide(2).negate());
+                    health.translateXProperty().bind(health.widthProperty().divide(2).negate());
                     break;
                 case "Spell":
                     character = new ImageView(new Image(card.getIdleSrc()));
@@ -40,6 +45,7 @@ public class CardView {
                     break;
             }
             type = new Label(card.getType());
+            name = new Label(card.getName());
             assert character != null;
             character.setId(Integer.toString(card.getId()));
             template.setFitHeight(Constants.CARD_HEIGHT);
@@ -48,27 +54,29 @@ public class CardView {
             type.setTextFill(Color.LIGHTCYAN);
             type.translateXProperty().bind(type.widthProperty().divide(2).negate());
             type.relocate(Constants.CARD_TYPE_X, Constants.CARD_TYPE_Y);
+            name.setFont(Font.font(Constants.INFO_FONT, FontWeight.EXTRA_BOLD, Constants.FONT_SIZE / 2));
+            name.setTextFill(Color.LIGHTCYAN);
+            name.translateXProperty().bind(name.widthProperty().divide(2).negate());
+            name.relocate(Constants.CARD_NAME_X, Constants.CARD_NAME_Y);
             if (!card.getType().equals("Spell")) {
                 assert power != null;
                 power.setTextFill(Color.YELLOW);
                 health.setTextFill(Color.RED);
                 power.setFont(Font.font(Constants.INFO_FONT, FontWeight.EXTRA_BOLD, Constants.FONT_SIZE));
                 health.setFont(Font.font(Constants.INFO_FONT, FontWeight.EXTRA_BOLD, Constants.FONT_SIZE));
-                power.setLayoutY(Constants.CARD_INFO_Y);
-                health.setLayoutY(Constants.CARD_INFO_Y);
-                power.setLayoutX(Constants.CARD_POWER_X);
-                health.setLayoutX(Constants.CARD_HEALTH_X);
+                power.relocate(Constants.CARD_POWER_X, Constants.CARD_INFO_Y);
+                health.relocate(Constants.CARD_HEALTH_X, Constants.CARD_INFO_Y);
                 character.setFitHeight(Constants.GIF_HEIGHT);
                 character.setFitWidth(Constants.GIF_WIDTH);
                 character.setLayoutX(Constants.CARD_WIDTH / 2 - Constants.GIF_WIDTH / 2);
-                pane.getChildren().addAll(template, character, type, power, health);
+                pane.getChildren().addAll(template, character, type, name, power, health);
 
             } else {
                 character.setFitHeight(Constants.GIF_HEIGHT / 2);
                 character.setFitWidth(Constants.GIF_WIDTH / 2);
                 character.setLayoutX(Constants.CARD_WIDTH / 2 - Constants.GIF_WIDTH / 4);
                 character.setLayoutY(Constants.GIF_HEIGHT / 4);
-                pane.getChildren().addAll(template, character, type);
+                pane.getChildren().addAll(template, character, type, name);
             }
             pane.setOnMouseClicked(event -> System.out.println(character.getId()));
         } catch (Exception e) {
@@ -92,7 +100,12 @@ public class CardView {
             type.setTextFill(Color.LIGHTCYAN);
             type.translateXProperty().bind(type.widthProperty().divide(2).negate());
             type.relocate(Constants.CARD_TYPE_X, Constants.CARD_TYPE_Y);
-            pane.getChildren().addAll(template, character, type);
+            name = new Label(item.getName());
+            name.setFont(Font.font(Constants.INFO_FONT, FontWeight.EXTRA_BOLD, Constants.FONT_SIZE / 2));
+            name.setTextFill(Color.LIGHTCYAN);
+            name.translateXProperty().bind(name.widthProperty().divide(2).negate());
+            name.relocate(Constants.CARD_NAME_X, Constants.CARD_NAME_Y);
+            pane.getChildren().addAll(template, character, type, name);
             pane.setOnMouseClicked(event -> System.out.println(character.getId()));
         } catch (Exception e) {
 
