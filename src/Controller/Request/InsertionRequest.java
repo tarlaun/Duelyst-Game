21f33e9adcf2cf2ac4c3504784a50fe2e102becc
neginjafariii <neginjfi79@gmail.com>
2019-07-06 10:05:18 +0430

    insertion request updated

--- src/Controller/Request/InsertionRequest.java
+++ src/Controller/Request/InsertionRequest.java
@@ -1,4 +1,27 @@
 package Controller.Request;
 
+import Model.Coordinate;
+
 public class InsertionRequest {
+
+    private int cardId;
+    private Coordinate coordinate;
+    public InsertionRequest(String ...args) {
+    }
+
+    public int getCardId() {
+        return cardId;
+    }
+
+    public void setCardId(int cardId) {
+        this.cardId = cardId;
+    }
+
+    public Coordinate getCoordinate() {
+        return coordinate;
+    }
+
+    public void setCoordinate(Coordinate coordinate) {
+        this.coordinate = coordinate;
+    }
 }
